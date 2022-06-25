b05c1bcba51c861279390eead71a575c8fb0f285
dfarfan <dfarfan@consisint.com>
2022-06-25 17:08:30 -0500

    Folder Organization

--- CxC Manual.sql
+++
@@ -1,7 +0,0 @@
---Revisión Job CxC Manual Vida Inversión
-
-select * from openitem where openitemid = 6440122749 ;
-
-select * from stps_batchprocessrecord_fund where id_object =  1485784901 ;
-
-select * from stca_openiteminfo where opm_id = 6440122749 ;
--- Polizas Anuladas VLVG.sql
+++
@@ -1,2 +0,0 @@
-select numcert, count(1)  FROM INT_ANULACION_AX@DBL_PROD  where codprod in  (8809, 8501, 8801, 8711 )
-  and stsretornoae = 1 group by numcert having count(1) > 1;
