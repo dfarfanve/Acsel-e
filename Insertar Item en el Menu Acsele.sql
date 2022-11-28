6e8234437a38fa6073be3081638dfc99a7fb64b4
dfarfan <53529167+dfarfanve@users.noreply.github.com>
2022-11-27 18:44:12 -0600

    Add files via upload

---
+++ Insertar Item en el Menu Acsele.sql
@@ -0,0 +1,6 @@
+INSERT INTO APP_VIDA_GU.CDSE_MENUITEM (MIT_ID, MIT_KEY, MIT_NAME, MIT_DESCRIPTION, MIT_ACTIONPATH, MIT_PARAMETERS, MIT_ACTIVE, MIT_ORDER, MIT_PARENTID, MIT_SECURITYENABLED, MEN_ID)
+VALUES
+  (APP_VIDA_GU.SQ_CDSE_MENUITEM.nextval, 'Menu2_5_17','Regenerar Recibos Pendientes','Regenerar Recibos Pendientes','/WController/vaadinservlet?view=cancelOpenItem',null,'1','180','85',null,'1',null,null,null,'1');
+
+COMMIT;
+
